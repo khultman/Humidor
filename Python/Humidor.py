783ad1123eec9e3edba9570e2a88f488478b6c3f
Kyle Hultman <1060151+khultman@users.noreply.github.com>
2017-08-03 09:23:26 -0400

    incremental adjustments/testing

--- Python/Humidor.py
+++ Python/Humidor.py
@@ -68,9 +68,9 @@ def disp_sensor_data(humidity = 0, temp_f = 0):
 
 
 def get_sensor_data():
-	temp_c = [None]*max(sensors)
-	temp_f = [None]*max(sensors)
-	humidity = [None]*max(sensors)
+	temp_c = [0]*sensors
+	temp_f = [0]*sensors
+	humidity = [0]*sensors
 	temp_c[sensors] = 0
 	temp_f[sensors] = 0
 	humidity[sensors] = 0
