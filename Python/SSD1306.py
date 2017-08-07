a3c731ec2a9f413aa0bdadc3158d1e5d53583848
Kyle Hultman <1060151+khultman@users.noreply.github.com>
2017-08-07 11:03:01 -0400

    incremental adjustments/testing

--- Python/SSD1306.py
+++ Python/SSD1306.py
@@ -15,7 +15,7 @@ import Adafruit_GPIO.SPI as SPI
 # Constants
 SSD1306_I2C_ADDRESS = 0x3C    # 011110+SA0+RW - 0x3C or 0x3D
 SSD1306_SETCONTRAST = 0x81
-SSD1306_DISPLAYALLON_RESUME = 0xA4
+SSD1306_DISPLAYALLONRESUME = 0xA4
 SSD1306_DISPLAYALLON = 0xA5
 SSD1306_NORMALDISPLAY = 0xA6
 SSD1306_INVERTDISPLAY = 0xA7
@@ -31,8 +31,6 @@ SSD1306_SETLOWCOLUMN = 0x00
 SSD1306_SETHIGHCOLUMN = 0x10
 SSD1306_SETSTARTLINE = 0x40
 SSD1306_MEMORYMODE = 0x20
-SSD1306_COLUMNADDR = 0x21
-SSD1306_PAGEADDR = 0x22
 SSD1306_COMSCANINC = 0xC0
 SSD1306_COMSCANDEC = 0xC8
 SSD1306_SEGREMAP = 0xA0
@@ -40,14 +38,17 @@ SSD1306_CHARGEPUMP = 0x8D
 SSD1306_EXTERNALVCC = 0x1
 SSD1306_SWITCHCAPVCC = 0x2
 
+SSD1306_COLUMNADDR = 0x21
+SSD1306_PAGEADDR = 0x22
+
 # Scrolling constants
-SSD1306_ACTIVATE_SCROLL = 0x2F
-SSD1306_DEACTIVATE_SCROLL = 0x2E
-SSD1306_SET_VERTICAL_SCROLL_AREA = 0xA3
-SSD1306_RIGHT_HORIZONTAL_SCROLL = 0x26
-SSD1306_LEFT_HORIZONTAL_SCROLL = 0x27
-SSD1306_VERTICAL_AND_RIGHT_HORIZONTAL_SCROLL = 0x29
-SSD1306_VERTICAL_AND_LEFT_HORIZONTAL_SCROLL = 0x2A
+SSD1306_ACTIVATESCROLL = 0x2F
+SSD1306_DEACTIVATESCROLL = 0x2E
+SSD1306_SETVERTICALSCROLLAREA = 0xA3
+SSD1306_RIGHTHORIZONTALSCROLL = 0x26
+SSD1306_LEFTHORIZONTALSCROLL = 0x27
+SSD1306_VERTICALRIGHTHORIZONTALSCROLL = 0x29
+SSD1306_VERTICALLEFTHORIZONTALSCROLL = 0x2A
 
 
 class SSD1306Base(object):
@@ -180,33 +181,7 @@ class SSD1306Base(object):
                 self._i2c.writeList(control, self._buffer[i:i+16])
 
 
-    def set_columnaddress(self, add = 0):
-        self.command((0x10|(add>>4))+0x02)
-        self.command((0x0f&add))
 
-    def set_memorymode(self, mode = 0):
-        self.command(0x20)
-        self.command(0)
-
-    def set_pageaddress(self, add = 0):
-        add = 0xb0|add
-        self.command(add)
-
-    def hard_clear(self, c = 0x00):
-        for n in [0, 1, 2, 3, 4, 5, 6, 7]:
-            self.set_pageaddress(n)
-            self.set_columnaddress(n)
-            for j in range(0, 0x80):
-                if self._spi is not None:
-                    self.spi_send(c)
-
-    def spi_send(self, data):
-        # Write buffer data.
-        if self._spi is not None:
-            # Set DC high for data.
-            self._gpio.set_high(self._dc)
-            # Write buffer.
-            self._spi.write(self._buffer)
 
 
     def image(self, image):
@@ -277,6 +252,7 @@ class SSD1306_64_48(SSD1306Base):
         super(SSD1306_64_48, self).__init__(64, 48, rst, dc, sclk, din, cs,
                                              gpio, spi, i2c_bus, i2c_address, i2c)
 
+
     def _initialize(self):
         # 64x48 pixel specific initialization.
         self.command(SSD1306_DISPLAYOFF)                    # 0xAE
@@ -296,7 +272,7 @@ class SSD1306_64_48(SSD1306Base):
         self.command(0x14)
 
         self.command(SSD1306_NORMALDISPLAY)                 # 0xA6
-        self.command(SSD1306_DISPLAYALLON_RESUME)           # 0xA4
+        self.command(SSD1306_DISPLAYALLONRESUME)           # 0xA4
 
         self.command(SSD1306_SEGREMAP | 0x1)
         self.command(SSD1306_COMSCANDEC)
@@ -312,4 +288,58 @@ class SSD1306_64_48(SSD1306Base):
 
         self.command(SSD1306_SETVCOMDETECT)                 # 0xDB
         self.command(0x40)
+
+    def set_columnaddress(self, add = 0):
+        self.command((0x10|(add>>4))+0x02)
+        self.command((0x0f&add))
+
+    def set_memorymode(self, mode = 0):
+        self.command(0x20)
+        self.command(0)
+
+    def set_pageaddress(self, add = 0):
+        add = 0xb0|add
+        self.command(add)
+
+    def hard_clear(self, c = 0x00):
+        for n in range(0,8):
+            self.set_pageaddress(n)
+            self.set_columnaddress(0)
+            for j in range(0, 0x80):
+                if self._spi is not None:
+                    self.spi_send(c)
+
+    def spi_send(self, data):
+        # Write buffer data.
+        if self._spi is not None:
+            # Set DC high for data.
+            self._gpio.set_high(self._dc)
+            # Write buffer.
+            self._spi.write(data)
+
+    def display(self):
+        """Write display buffer to physical display."""
+
+        # set column address
+        self.set_columnaddress(0)
+
+        # set memory mode
+        #self.set_memorymode(0)
+
+        self.set_pageaddress(0)
+
+        #self.command(0x21)
+        #self.command(0)              # Column start address. (0 = reset)
+        #self.command(self.width-1)   # Column end address.
+
+        #self.command(0x22)
+        #self.command(0)              # Page start address. (0 = reset)
+        #self.command(self._pages)  # Page end address.
+
+        # Write buffer data.
+        for i in range (6):
+            self.set_pageaddress(i)
+            self.set_columnaddress(0)
+            for j in range(0x40):
+                self.spi_send(self._buffer[i*0x40+j])
         
