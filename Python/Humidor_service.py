79fc27ba4778748fbb9eaebd0b3f25280553d971
Kyle Hultman <1060151+khultman@users.noreply.github.com>
2017-08-08 10:43:28 -0400

    lowering the sleep timer in i2c commands

--- Python/Humidor_service.py
+++ Python/Humidor_service.py
@@ -1,39 +1,28 @@
 
+import argparse
 
 from Humidor import Humidor
-
-import argparse
 from logger import MLOGGER
 
 import RPi.GPIO as GPIO
-
 import time
 
 
-
-threshold = getattr(logging, loglevel.upper(), None)
-if not isinstance(threshold, int):
-    raise ValueError("Invalid log level: {0}".format(loglevel))
-logging.basicConfig(filename='Humidor.log',level=threshold)
-logger = logging.getLogger('Humidor.py')
-
 # The I2C Bus ID
 busID = 1
-
 # GPIO of SSD1306 Reset pin
 RST = 24
-
 # GPIO of SSD1306 Display DC
 DC = 23
-
 # SPI Port & Device
 SPI_PORT = 0
 SPI_DEVICE = 0
-
 # The number of sensors, min 1 max 8 
 # corresponds to TCA9548 Channels
 # Sensors must start on channel 0 and increment from there
 sensors = 3
+
+
 
 def get_cli_args(args=None):
 	parser = argparse.ArgumentParser(description='Run the Humidor service')
