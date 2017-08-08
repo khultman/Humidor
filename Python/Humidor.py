7187af3f02e055a82f7b422f79d5df3030ac7e90
Kyle Hultman <1060151+khultman@users.noreply.github.com>
2017-08-08 09:59:54 -0400

    splitting Humidor into separate class

--- Python/Humidor.py
+++ Python/Humidor.py
@@ -14,8 +14,6 @@ from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
 
-import RPi.GPIO as GPIO
-
 from SI7021 import SI7021
 from SSD1306 import SSD1306_64_48
 from TCA9548A import TCA9548A
@@ -24,125 +22,114 @@ from smbus2 import SMBus
 
 import time
 
-# Setup logging
-logger = logging.getLogger('Humidor.py')
-threshold = getattr(logging, loglevel.upper(), None)
-if not isinstance(threshold, int):
-    raise ValueError("Invalid log level: {0}".format(loglevel))
-logger.basicConfig(filename='Humidor.log',level=threshold)
+class Humidor(object):
+	def __init__(self, i2cBUS = 1, sensors = 1, rst = 24, dc = 23, spiPort = 0, spiDevice = 0):
+		self._log = logging.getLogger('Humidor.Humidor')
+		self._degS = u'\N{DEGREE SIGN}'
+		self._busID = i2cBUS
+		self._rst = rst
+		self._dc = dc
+		self._spiPort = spiPort
+		self._spiDevice = spiDevice
+		self.sensors = sensors
+		self.multiplexor = TCA9548A(SMBus(self._busID), 0.1)
+		self.sensor = SI7021(SMBus(self._busID), 0.1)
+		self.disp = disp = SSD1306_64_48(rst=RST, dc=DC, spi=SPI.SpiDev(self.spiPort, self.spiDevice, max_speed_hz=8000000))
 
-# The I2C Bus ID
-busID = 1
+	def _clear(self):
+		self._sensor_data = [None] * 3
 
-# GPIO of SSD1306 Reset pin
-RST = 24
+	# Shorthand method for displaying the averaged sensor data
+	def disp_avg_sensor_data(self):
+		self.disp_sensor_data(self._sensor_data[2][self.sensors], self._sensor_data[1][self.sensors], self._sensor_data[0][self.sensors])
 
-# GPIO of SSD1306 Display DC
-DC = 23
+	# Write the given sensor data to the screen
+	def disp_sensor_data(self, humidity = 0, temp_f = 0, temp_c = 0):
+		disp.begin()
+		disp.clear()
+		disp.display()
+		width = disp.width
+		height = disp.height
+		image = Image.new('1', (width, height))
 
-# SPI Port & Device
-SPI_PORT = 0
-SPI_DEVICE = 0
+		padding = 2
+		top = padding
+		bottom = height-padding
+		x = padding
+		fontsize = 14
 
-# The number of sensors, min 1 max 8 
-# corresponds to TCA9548 Channels
-# Sensors must start on channel 0 and increment from there
-sensors = 3
+		humidity = "{0}%".format(round(humidity, 2))
+		temp_f = "{0}{1}F".format(round(temp_f, 2),self._degS)
+		temp_c = "{0}{1}C".format(round(temp_c, 2),self._degS)
 
-disp = SSD1306_64_48(rst=RST, dc=DC, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=8000000))
-multiplexor = TCA9548A(SMBus(busID))
-sensor = SI7021(SMBus(busID))
+		draw = ImageDraw.Draw(image)
+		fontRH = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", fontsize)
+		fontT = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", fontsize-4)
 
-degS = u'\N{DEGREE SIGN}'
+		draw.text((x+1, top+4), humidity,  font=fontRH, fill=255)
+		draw.text((x+6, top+20), temp_f, font=fontT, fill=255)
+		draw.text((x+6, top+30), temp_c, font=fontT, fill=255)
 
+		disp.image(image)
+		disp.display()
 
-def disp_sensor_data(humidity = 0, temp_f = 0, temp_c = 0):
-	disp.begin()
-	disp.clear()
-	disp.display()
-	width = disp.width
-	height = disp.height
-	image = Image.new('1', (width, height))
+	# Reads the sensor data from all sensor channels
+	# Returns multidimensional list for external processing
+	# Stores data to self._sensor_data for internal processing
+	def get_sensor_data(self):
+		temp_c = [None]*(sensors + 1)
+		temp_f = [None]*(sensors + 1)
+		humidity = [None]*(sensors + 1)
+		temp_c[sensors] = 0
+		temp_f[sensors] = 0
+		humidity[sensors] = 0
 
-	padding = 2
-	top = padding
-	bottom = height-padding
-	x = padding
-	fontsize = 14
+		for i in range(0, sensors):
+			multiplexor.select_channel(i)
+			temp_c[i] = sensor.get_temperature_c()
+			temp_f[i] = sensor.get_temperature_f()
+			humidity[i] = sensor.get_humidity()
+			self._log.debug("Channel {0}, temp_c {1}, temp_f {2}, humidity {3}".format(i, temp_c[i], temp_f[i], humidity[i]))
+			temp_c[sensors] += temp_c[i]
+			temp_f[sensors] += temp_f[i]
+			humidity[sensors] += humidity[i]
 
-	humidity = "{0}%".format(round(humidity, 2))
-	temp_f = "{0}{1}F".format(round(temp_f, 2),degS)
-	temp_c = "{0}{1}C".format(round(temp_c, 2),degS)
+		temp_c[sensors] = temp_c[sensors] / sensors
+		temp_f[sensors] = temp_f[sensors] / sensors
+		humidity[sensors] = humidity[sensors] / sensors
 
-	draw = ImageDraw.Draw(image)
-	fontRH = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", fontsize)
-	fontT = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", fontsize-4)
+		self._sensor_data = [temp_c, temp_f, humidity]
+		return [temp_c, temp_f, humidity]
 
-	draw.text((x+1, top+4), humidity,  font=fontRH, fill=255)
-	draw.text((x+6, top+20), temp_f, font=fontT, fill=255)
-	draw.text((x+6, top+30), temp_c, font=fontT, fill=255)
-
-	disp.image(image)
-	disp.display()
-
-
-def get_sensor_data():
-	temp_c = [None]*(sensors + 1)
-	temp_f = [None]*(sensors + 1)
-	humidity = [None]*(sensors + 1)
-	temp_c[sensors] = 0
-	temp_f[sensors] = 0
-	humidity[sensors] = 0
-
-	for i in range(0, sensors):
-		multiplexor.select_channel(i)
-		temp_c[i] = sensor.get_temperature_c()
-		temp_f[i] = sensor.get_temperature_f()
-		humidity[i] = sensor.get_humidity()
-		logger.debug("Channel {0}, temp_c {1}, temp_f {2}, humidity {3}".format(i, temp_c[i], temp_f[i], humidity[i]))
-		temp_c[sensors] += temp_c[i]
-		temp_f[sensors] += temp_f[i]
-		humidity[sensors] += humidity[i]
-
-	temp_c[sensors] = temp_c[sensors] / sensors
-	temp_f[sensors] = temp_f[sensors] / sensors
-	humidity[sensors] = humidity[sensors] / sensors
-
-	return [temp_c, temp_f, humidity]
-
-
-def read(channel = 0):
-	multiplexor.select_channel(channel)
-	temp_c = sensor.get_temperature_c()
-	temp_f = sensor.get_temperature_f()
-	humidity = sensor.get_humidity()
-	logger.debug("Channel {0}, temp_c {1}, temp_f {2}, humidity {3}".format(channel, temp_c, temp_f, humidity))
-	print("Statistics for sensor on channel : %d" %channel)
-	print("Relative Humidity is : {0}%".format(round(humidity,2)))
-	print("Temperature in Celsius is : {0}C".format(round(temp_c,2)))
-	print("Temperature in Fahrenheit is : {0}F".format(round(temp_f,2)))
-	print("")
-
-
-def print_sensor_data(sensor_data = []):
-	for i in range(0, sensors):
-		print("Sensor data for channel {0}".format(i))
-		print("Relative Humidity is {0}%".format( round(sensor_data[2][i],2) ))
-		print("Temperatur in Celsius is {0}{1} C".format( round(sensor_data[0][i],2), degS ))
-		print("Temperature in Fahrenheit is {0}{1} F".format( round(sensor_data[1][i],2), degS ))
+	# Read the sensor data from an individual channel
+	def read(self, channel = 0):
+		multiplexor.select_channel(channel)
+		temp_c = sensor.get_temperature_c()
+		temp_f = sensor.get_temperature_f()
+		humidity = sensor.get_humidity()
+		self._log.debug("Channel {0}, temp_c {1}, temp_f {2}, humidity {3}".format(channel, temp_c, temp_f, humidity))
+		print("Statistics for sensor on channel : %d" %channel)
+		print("Relative Humidity is : {0}%".format(round(humidity,2)))
+		print("Temperature in Celsius is : {0}C".format(round(temp_c,2)))
+		print("Temperature in Fahrenheit is : {0}F".format(round(temp_f,2)))
 		print("")
 
-	print("Averaged sensor data")
-	print("Relative Humidity is {0}%".format( round(sensor_data[2][sensors],2) ))
-	print("Temperatur in Celsius is {0}{1} C".format( round(sensor_data[0][sensors],2), degS ))
-	print("Temperature in Fahrenheit is {0}{1} F".format( round(sensor_data[1][sensors],2), degS ))
-	print("")
 
-try:
-	while True:
-		sensor_data = get_sensor_data()
-		print_sensor_data(sensor_data)
-		disp_sensor_data(sensor_data[2][sensors], sensor_data[1][sensors], sensor_data[0][sensors])
-except KeyboardInterrupt:
-	GPIO.cleanup()
+	# Writes the data stored in self._sensor_data to stdout
+	def print_sensor_data(self):
+		for i in range(self._sensor_data - 1):
+			print("Sensor data for channel {0}".format(i))
+			print("Relative Humidity is {0}%".format( round(self._sensor_data[2][i],2) ))
+			print("Temperatur in Celsius is {0}{1} C".format( round(self._sensor_data[0][i],2), self._degS ))
+			print("Temperature in Fahrenheit is {0}{1} F".format( round(self._sensor_data[1][i],2), self._degS ))
+			print("")
+
+		print("Averaged sensor data")
+		print("Relative Humidity is {0}%".format( round(self._sensor_data[2][sensors],2) ))
+		print("Temperatur in Celsius is {0}{1} C".format( round(self._sensor_data[0][sensors],2), self._degS ))
+		print("Temperature in Fahrenheit is {0}{1} F".format( round(self._sensor_data[1][sensors],2), self._degS ))
+		print("")
+
+
+
 
